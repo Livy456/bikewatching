c439fea86060d7af4b4bfce2bd47ac32c15a7500
Olivia <52472697+Livy456@users.noreply.github.com>
2024-04-11 09:31:42 -0400

    update +page.svelte and add bluebikes-stations.csv

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
     import "../../node_modules/mapbox-gl/dist/mapbox-gl.css" 
     import { onMount } from "svelte";
 
+    let bike_styling = {line_width: 2, line_color: "green", line_opacity: 0.4}
+
     mapboxgl.accessToken = "pk.eyJ1Ijoib2xpdmlhOTg3IiwiYSI6ImNsdXVzZ3R6bTBkY2wyaW5reG0zYXp2ZmQifQ.7knWre2RB222IHPl2qrgjg";
 
     onMount( async() => {
@@ -19,16 +21,31 @@
             data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D",
         });
 
+        map.addSource("cambridge_route", {
+            type: "geojson",
+            data: "Cambridge_Bike_Lanes.geojson",
+        })
+
         map.addLayer({
             id:'boston_bike_layer',
             type: 'line',
             source: "boston_route",
             paint: {
-                'line-color': "green",
-                'line-width': 2,
-                'line-opacity': 0.4,
+                'line-color': bike_styling.line_color,
+                'line-width': bike_styling.line_width,
+                'line-opacity': bike_styling.line_opacity,
+            }
+        });
 
-            },
+        map.addLayer({
+            id: "cambridge_bike_layer",
+            type: "line",
+            source: "cambridge_route",
+            paint: {
+                'line-color': bike_styling.line_color,
+                'line-width': bike_styling.line_width,
+                'line-opacity': bike_styling.line_opacity,
+            }
         });
     });
 </script>
